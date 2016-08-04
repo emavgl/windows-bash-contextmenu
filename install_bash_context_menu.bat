40cd30591550ff6fd68a3fd4dc5c9d59827e7cb9
Emanuele Viglianisi <emavgl@gmail.com>
2016-08-04 20:30:18 +0200

    missing %

--- install_bash_context_menu.bat
+++ install_bash_context_menu.bat
@@ -1,12 +1,12 @@
 @ECHO OFF
 REG ADD HKCR\Directory\shell\bash\ /ve /d "Open in Bash"
-REG ADD HKCR\Directory\shell\bash\command /ve /d "cmd.exe /s /k pushd \"%V\" & bash.exe"
+REG ADD HKCR\Directory\shell\bash\command /ve /d "cmd.exe /s /k pushd \"%%V\" & bash.exe"
 REG ADD HKCR\Directory\shell\bash /v icon /d "%USERPROFILE%\AppData\Local\lxss\bash.ico"
 
 REG ADD HKCR\Drive\shell\bash\ /ve /d "Open in Bash"
-REG ADD HKCR\Drive\shell\bash\command /ve /d "cmd.exe /s /k pushd \"%V\" & bash.exe"
+REG ADD HKCR\Drive\shell\bash\command /ve /d "cmd.exe /s /k pushd \"%%V\" & bash.exe"
 REG ADD HKCR\Drive\shell\bash /v icon /d "%USERPROFILE%\AppData\Local\lxss\bash.ico"
 
 REG ADD HKCR\Directory\Background\shell\bash\ /ve /d "Open in Bash"
-REG ADD HKCR\Directory\Background\shell\bash\command /ve /d "cmd.exe /s /k pushd \"%V\" & bash.exe"
+REG ADD HKCR\Directory\Background\shell\bash\command /ve /d "cmd.exe /s /k pushd \"%%V\" & bash.exe"
 REG ADD HKCR\Directory\Background\shell\bash /v icon /d "%USERPROFILE%\AppData\Local\lxss\bash.ico"

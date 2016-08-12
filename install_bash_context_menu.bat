6cea3d104d7e6359b66c857d47b1cfafc0708d69
Emanuele Viglianisi <emavgl@gmail.com>
2016-08-12 18:30:22 +0200

    new terminal dedicated

--- install_bash_context_menu.bat
+++ install_bash_context_menu.bat
@@ -1,12 +1,19 @@
 @ECHO OFF
+
+SET mypath=%~dp0
+cd %mypath:~0,-1%
+
+set "command=cmd.exe /s /k pushd \"%%V\" & start \"\" \"%USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\BashUbuntu.lnk\" & exit"
+copy BashUbuntu.lnk "%USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\BashUbuntu.lnk"
+
 REG ADD HKCR\Directory\shell\bash\ /ve /d "Open in Bash"
-REG ADD HKCR\Directory\shell\bash\command /ve /d "cmd.exe /s /k pushd \"%%V\" & bash.exe"
+REG ADD HKCR\Directory\shell\bash\command /ve /d "%command%"
 REG ADD HKCR\Directory\shell\bash /v icon /d "%USERPROFILE%\AppData\Local\lxss\bash.ico"
 
 REG ADD HKCR\Drive\shell\bash\ /ve /d "Open in Bash"
-REG ADD HKCR\Drive\shell\bash\command /ve /d "cmd.exe /s /k pushd \"%%V\" & bash.exe"
+REG ADD HKCR\Drive\shell\bash\command /ve /d "%command%"
 REG ADD HKCR\Drive\shell\bash /v icon /d "%USERPROFILE%\AppData\Local\lxss\bash.ico"
 
 REG ADD HKCR\Directory\Background\shell\bash\ /ve /d "Open in Bash"
-REG ADD HKCR\Directory\Background\shell\bash\command /ve /d "cmd.exe /s /k pushd \"%%V\" & bash.exe"
+REG ADD HKCR\Directory\Background\shell\bash\command /ve /d "%command%"
 REG ADD HKCR\Directory\Background\shell\bash /v icon /d "%USERPROFILE%\AppData\Local\lxss\bash.ico"
